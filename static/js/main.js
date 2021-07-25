1f15ba0caedeb3e293f7d8f25e4fbe846325f3e9
aleksandr.kirillov <aleksandr.kirillov@myposter.de>
2021-07-25 10:02:15 +0200

    fill the list with js

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,30 @@
-const data = {}
+class Element {
+    constructor(text, checked) {
+        this.text = text;
+        this.checked = checked;
+    }
+    text
+    checked
+}
+
+const data = [
+    new Element("bread", false),
+    new Element("butter", true),
+    new Element("milk", false),
+]
 
 const root = document.getElementById("root");
-let label = document.createElement("label");
-label.className = "flex-middle";
-let inputElement = document.createElement("input");
-inputElement.type = "checkbox"
-label.appendChild(inputElement)
-label.append(document.createTextNode("Bananas"))
-// label.innerText = "Bananas"
-root.appendChild(label);
+
+data.forEach(
+    (element) => {
+        let label = document.createElement("label");
+        label.className = "flex-middle";
+        let inputElement = document.createElement("input");
+        inputElement.type = "checkbox"
+        inputElement.checked = element.checked
+        label.appendChild(inputElement)
+        label.append(document.createTextNode(element.text))
+        root.appendChild(label);
+    }
+)
+
